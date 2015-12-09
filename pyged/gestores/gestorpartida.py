ec62763ca800062043279b94aee03c8c0ecfafd3
MatiBlanco <matucolon@hotmail.com>
2015-12-08 23:06:39 -0300

    Arrgelado listar_partida, agregado listar_resultado y agregar_resultado

--- pyged/gestores/gestorpartida.py
+++ pyged/gestores/gestorpartida.py
@@ -1,6 +1,10 @@
 from main import Singleton
 from gestorbasededatos import GestorBaseDeDatos
-from dtos import DTOPartida
+from dtos import *
+from pyged.almacenamiento import *
+from datetime import datetime
+from excepciones import *
+from gestorcompetencia import GestorCompetencia
 
 class GestorPartida(Singleton):
     """Realiza tareas correspondientes al manejo de clases Lugar"""
@@ -24,3 +28,55 @@ class GestorPartida(Singleton):
                              competencia.tipo_puntuacion, competencia.cantidad_de_sets)
             lista_dtos.append(dto)
         return lista_dtos
+
+    def listar_resultado(self, id_partida):
+        partida = GestorBaseDeDatos.get_instance().listar_partida(id_partida = id_partida)
+        if partida.resultado is None:
+            raise FaltaDeDatos('No existen Resultados para esta partida.')
+        if partida.resultado.tipo == 'porsets':
+            lista_sets = []
+            for sets in partida.resultado.sets:
+                dtoSet = DTOSet(sets.id, sets.puntaje_de_local, sets.puntaje_de_visitante,sets.numero)
+                lista_sets.append(dtoSet)
+            return DTOResultado(partida.resultado.id, partida.id, partida.resultado.tipo,partida.local_presente, partida.visitante_presente,
+                                None, None, lista_sets)
+        if partida.resultado.tipo == 'porresultadofinal':
+            return DTOResultado(partida.resultado.id, partida.id, partida.resultado.tipo,partida.local_presente,partida.visitante_presente,
+                               partida.resultado.resultado_de_local, partida.resultado.resultado_de_visitante, None)
+        if partida.resultado.tipo == 'porpuntuacion':
+            return DTOResultado(partida.resultado.id, partida.id, partida.resultado.tipo,partida.local_presente,partida.visitante_presente,
+                               partida.resultado.puntos_de_local, partida.resultado.puntos_de_visitante, None)
+    def agregar_resultado(self, dto):
+        partida = GestorBaseDeDatos.get_instance().listar_partidas(id_partida = dto.id_partida)
+        if dto.tipo == 'porsets':
+            lista_de_sets = []
+            for dtoset in dto.lista_dto_sets:
+                new_set = Set( puntaje_de_local = dtoset.puntaje_local, puntaje_de_visitante = dtoset.puntaje_visitante,
+                          numero = dtoset.numero_de_set)
+                lista_de_sets.append(new_set)
+            resultado_new = ResultadoPorSet(fecha = datetime.now().date(), sets = lista_de_sets)
+        elif dto.tipo == 'porresultadofinal':
+            resultado_new = ResultadoPorResultadoFinal(fecha = datetime.now().date(), resultado_de_local = dto.resultado_local,
+                resultado_de_visitante = dto.resultado_visitante)
+        elif dto.tipo == 'porpuntuacion':
+            resultado_new = ResultadoPorResultadoFinal(fecha = datetime.now().date(), puntos_de_local = dto.resultado_local,
+                puntos_de_visitante = dto.resultado_visitante)
+        if partida.resultado is None:
+            dtocompetencia = DTOCompetencia(partida.id_competencia, None, None, 'En Disputa',None, None, None, None, None, None,None,
+                                 None, None, None, None, None)
+            GestorCompetencia.get_instance().modificar_competencia(dtocompetencia)
+        partida.resultado = resultado_new
+        partida.visitante_presente = dto.visitante_presente
+        partida.local_presente = dto.local_presente
+        partida.estado = 'Finalizada'
+        partida.historial.append(resultado_new)
+        GestorBaseDeDatos.get_instance().modificar_partida()
+        lista_partidas = GestorBaseDeDatos.get_instance().listar_partidas(id_competencia = partida.id_competencia)
+        terminado = 0
+        for partidas in lista_partidas:
+            if partidas.resultado is not None:
+                terminado += 1
+        if terminado == len(lista_partidas):
+            dtocompetencia = DTOCompetencia(partida.id_competencia,None, None, 'Finalizada', None, None, None, None, None, None, None,
+                             None, None, None, None, None)
+            GestorCompetencia.get_instance().modificar_competencia(dtocompetencia)
