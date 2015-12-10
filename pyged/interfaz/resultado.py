777954d0049d2dc6c38db40e379e615e0d0f6508
Juxtar <julianema6@gmail.com>
2015-12-10 00:09:53 -0300

    Corregido MostrarFixture e implementados los detallar resultados

--- pyged/interfaz/resultado.py
+++ pyged/interfaz/resultado.py
@@ -4,6 +4,7 @@ import gtk
 import gtk.glade
 from os import path
 from pyged.gestores.gestorpartida import GestorPartida
+from pyged.gestores.gestorcompetencia import GestorCompetencia
 from pyged.gestores.dtos import DTOResultado, DTOSet
 from pyged.gestores.excepciones import FaltaDeDatos
 from main import agregar_cuadro_error, Interfaz
@@ -22,24 +23,21 @@ class MostrarFixture(Interfaz):
         self.glade.get_object('button3').connect('clicked', self.gestionar_resultado)
 
         combo_instancia = self.glade.get_object('combobox1')
-        combo_instancia.connect('changed', self.dinamizar)
         combo_equipo =  self.glade.get_object('combobox2')
-        combo_equipo.connect('changed', self.dinamizar)
 
-        datos_partidas = GestorPartida.get_instance().listar_partidas(id_competencia=id_competencia)
-        self.lista_partidas = []
+        self.lista_partidas = GestorPartida.get_instance().listar_partidas(id_competencia=id_competencia)
         instancias = set()
         equipos = set()
-        for dto in datos_partidas:
+        for dto in self.lista_partidas:
             equipos.add(dto.nombre_local)
             equipos.add(dto.nombre_visitante)
-            instancias.add(dto.instancia)
+            instancias.add('Fecha '+str(dto.instancia))
             try:
                 resultado = GestorPartida.get_instance().listar_resultado(dto.id)
             except FaltaDeDatos:
+                dto.ganador = 'Sin jugar'
                 continue
             if resultado.tipo == 'porresultadofinal':
-                dto.tipo_punt = 'porresultadofinal'
                 if resultado.resultado_local == 1.0:
                     dto.ganador = dto.nombre_local
                 elif resultado.resultado_local == 0.0:
@@ -47,7 +45,6 @@ class MostrarFixture(Interfaz):
                 else:
                     dto.ganador = 'Empate'
             elif resultado.tipo == 'porpuntuacion':
-                dto.tipo_punt = 'porpuntuacion'
                 if resultado.resultado_local > resultado.resultado_visitante:
                     dto.ganador = dto.nombre_local
                 elif resultado.resultado_local < resultado.resultado_visitante:
@@ -55,7 +52,6 @@ class MostrarFixture(Interfaz):
                 else:
                     dto.ganador = 'Empate'
             elif resultado.tipo == 'porsets':
-                dto.tipo_punt = 'porsets'
                 ganadores = []
                 for r_set in resultado.lista_dto_sets:
                     ganadores.append(r_set.puntaje_local>r_set.puntaje_visitante)
@@ -68,38 +64,52 @@ class MostrarFixture(Interfaz):
         self.treeview = self.glade.get_object('treeview1')
         self.treeview.get_model().clear()
         for partida in self.lista_partidas:
-            self.treeview.get_model().append([partida.instancia, partida.nombre_local, partida.nombre_visitante,
-                                              partida.ganador, partida.id])
+            self.treeview.get_model().append(['Fecha '+str(partida.instancia), partida.nombre_local,
+                                              partida.nombre_visitante, partida.ganador, partida.id])
 
         combo_instancia.get_model().clear()
+        combo_instancia.append_text('<<Todas>>')
         for x in instancias:
             combo_instancia.append_text(x)
+        combo_instancia.set_active(0)
         combo_equipo.get_model().clear()
+        combo_equipo.append_text('<<Todos>>')
         for x in equipos:
             combo_equipo.append_text(x)
+        combo_equipo.set_active(0)
 
         self.main_window = self.glade.get_object('fixture')
         self.main_window.connect('destroy', self.destroy)
         self.infobar, boton_cerrar = agregar_cuadro_error(self.main_window)
         boton_cerrar.connect('clicked', self.cerrar_error)
         self.main_window.show_all()
+        combo_instancia.connect('changed', self.dinamizar)
+        combo_equipo.connect('changed', self.dinamizar)
 
     def dinamizar(self, widget):
         nombre = gtk.Buildable.get_name(widget)
         if nombre == 'combobox1':
+            self.treeview.get_model().clear()
             if widget.get_active():
-                self.treeview.get_model().clear()
                 for partida in self.lista_partidas:
-                    if partida.instancia == widget.get_model()[widget.get_active()][0]:
-                        self.treeview.get_model().append([partida.instancia, partida.nombre_local,
+                    if 'Fecha '+str(partida.instancia) == widget.get_model()[widget.get_active()][0]:
+                        self.treeview.get_model().append(['Fecha '+str(partida.instancia), partida.nombre_local,
                                                           partida.nombre_visitante, partida.ganador, partida.id])
+            else:
+                for partida in self.lista_partidas:
+                    self.treeview.get_model().append(['Fecha '+str(partida.instancia), partida.nombre_local,
+                                                      partida.nombre_visitante, partida.ganador, partida.id])
         if nombre == 'combobox2':
+            self.treeview.get_model().clear()
             if widget.get_active():
-                self.treeview.get_model().clear()
                 for partida in self.lista_partidas:
                     if widget.get_model()[widget.get_active()][0] in [partida.nombre_local, partida.nombre_visitante]:
-                        self.treeview.get_model().append([partida.instancia, partida.nombre_local,
+                        self.treeview.get_model().append(['Fecha '+str(partida.instancia), partida.nombre_local,
                                                           partida.nombre_visitante, partida.ganador, partida.id])
+            else:
+                for partida in self.lista_partidas:
+                    self.treeview.get_model().append(['Fecha '+str(partida.instancia), partida.nombre_local,
+                                                      partida.nombre_visitante, partida.ganador, partida.id])
 
     def ver_detalle(self, widget):
         pass
@@ -110,7 +120,7 @@ class MostrarFixture(Interfaz):
         cursor, _ = self.treeview.get_cursor()
         id_partida = model.get_value(model.get_iter(cursor), 4)
         dto_partida_seleccionada = filter(lambda x: x.id == id_partida, self.lista_partidas)[0]
-        n = opc[dto_partida_seleccionada.tipo_punt](id_partida, self.main_window)
+        n = opc[dto_partida_seleccionada.tipo_puntuacion](id_partida, self.main_window)
         self.main_window.hide()
 
     def destroy(self, widget):
@@ -321,3 +331,92 @@ class GestionarSets(Interfaz):
         if len(texto) > 2:
             texto = texto[:2]
         widget.set_text(texto)
+
+class MostrarResultadoPuntos:
+    """Interfaz para mostrar un resultado de tipo puntuacion"""
+    def __init__(self, id_partida):
+        self.id_partida = id_partida
+        self.glade = gtk.Builder()
+        self.glade.add_from_file(path.dirname( path.abspath(__file__) )+'\glade\\resultado.glade')
+        self.glade.get_object('button5').connect('clicked', self.volver)
+
+        datos_partida = GestorPartida.get_instance().listar_partidas(id_partida=id_partida)[0]
+        datos_resultado = GestorPartida.get_instance().listar_resultado(id_partida)
+        self.glade.get_object('label36').set_text(datos_partida.nombre_local)
+        self.glade.get_object('label37').set_text(datos_partida.nombre_visitante)
+        self.glade.get_object('label35').set_text(str(datos_resultado.resultado_local))
+        self.glade.get_object('label34').set_text(str(datos_resultado.resultado_visitante))
+
+        self.main_window = self.glade.get_object('resultado_puntos')
+        self.main_window.connect('destroy', self.destroy)
+        self.main_window.show_all()
+
+    def destroy(self, widget):
+        self.volver(None)
+
+    def volver(self, widget):
+        self.main_window.hide()
+
+class MostrarResultadoSets:
+    """Interfaz para mostrar un resultado de tipo sets"""
+    def __init__(self, id_partida):
+        self.id_partida = id_partida
+        self.glade = gtk.Builder()
+        self.glade.add_from_file(path.dirname( path.abspath(__file__) )+'\glade\\resultado.glade')
+        self.glade.get_object('button4').connect('clicked', self.volver)
+
+        datos_partida = GestorPartida.get_instance().listar_partidas(id_partida=id_partida)[0]
+        datos_resultado = GestorPartida.get_instance().listar_resultado(id_partida)
+        self.glade.get_object('label14').set_text(datos_partida.nombre_local)
+        self.glade.get_object('label15').set_text(datos_partida.nombre_visitante)
+
+        lista_widgets = [
+            {
+             'local': self.glade.get_object('label'+str(index)),
+             'visitante': self.glade.get_object('label'+str(index+9))
+            }
+                        for index in range(16, 25)]
+        for dto_set in datos_resultado.lista_dto_sets:
+            lista_widgets[dto_set.numero_de_set-1]['local'].set_text(str(dto_set.puntaje_local))
+            lista_widgets[dto_set.numero_de_set-1]['visitante'].set_text(str(dto_set.puntaje_visitante))
+
+        self.main_window = self.glade.get_object('resultado_sets')
+        self.main_window.connect('destroy', self.destroy)
+        self.main_window.show_all()
+
+    def destroy(self, widget):
+        self.volver(None)
+
+    def volver(self, widget):
+        self.main_window.hide()
+
+class MostrarTablaPosiciones:
+    """Interfaz para mostrar un resultado de tipo puntuacion"""
+    def __init__(self, id_competencia):
+        self.id_competencia = id_competencia
+        self.glade = gtk.Builder()
+        self.glade.add_from_file(path.dirname( path.abspath(__file__) )+'\glade\\resultado.glade')
+        self.glade.get_object('button5').connect('clicked', self.volver)
+
+        self.treeview = self.glade.get_object('treeview2')
+        self.treeview.get_model().clear()
+        datos_tabla = GestorCompetencia.get_instance().generar_tabla_posiciones(id_competencia)
+        for dto in datos_tabla:
+            datos = [
+                dto.nombre, dto.puntos, dto.partidos_ganados, dto.partidos_empatados, dto.partidos_perdidos,
+                dto.goles_a_favor, dto.goles_en_contra, dto.goles_a_favor-dto.goles_en_contra,
+                dto.partidos_empatados+dto.partidos_ganados+dto.partidos_perdidos
+            ]
+            for i, dato in enumerate(datos):
+                datos[i] = str(dato)
+            self.treeview.get_model().append(datos)
+
+        self.main_window = self.glade.get_object('tabla_posiciones')
+        self.main_window.connect('destroy', self.destroy)
+        self.main_window.show_all()
+
+    def destroy(self, widget):
+        self.volver(None)
+
+    def volver(self, widget):
+        self.main_window.hide()
